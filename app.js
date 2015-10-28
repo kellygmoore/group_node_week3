72bf850499399f0136c2d6a20824f530aec11868
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-10-28 16:51:15 -0500

    link

--- app.js
+++ app.js
@@ -4,10 +4,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var person = require('./person.js');
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.get('') NEED URL FROM AJAX CALL
+app.get('/bluehat'), function(request, response){
+    response.send(console.log("hello!"));
+    //response.send(person);
+};
+
 
 app.get("/*", function(request,response) {
     var file = request.params[0] || "index.html";
--- server/person.js
+++ server/person.js
@@ -1,3 +1,17 @@
 /**
  * Created by danesmith on 10/28/15.
  */
+var name = require('./name.js');
+var job = require('./job.js');
+var skill = require('./skill.js');
+
+
+function Employee(name, job, skill){
+    this.name = name;
+    this.job = job;
+    this.skill = skill;
+}
+
+var makeEmployee = new Employee(name, job, skill);
+
+module.exports = makeEmployee;
