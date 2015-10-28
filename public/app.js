017133c88ccc8053618ca62576ad6be2e49a4109
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2015-10-28 16:20:23 -0500

    add assignstaff function

--- public/app.js
+++ public/app.js
@@ -3,16 +3,17 @@
 var frontEndReq, backEndReq, logicReq;
 
 //number of employees in each category
-var backEndEmployee, frontEndEmployee, logicEmployee;
+var backEndEmployee = 0, frontEndEmployee = 0, logicEmployee = 0;
 
 //total number of points in each scrum category
 var frontEndTotal, backEndTotal, logicTotal;
 
 $(document).ready(function(){
     $("#genButtonDiv").on('click', generateProject)
-
+    $("#projectOverviewDiv").on('click', '#assignStaffBtn', assignStaff)
 
 });
+
 
 
 
@@ -24,10 +25,18 @@ function generateProject(){
     $("#projectOverviewDiv").append("<div id='frontEnd' class='taskBox'>Front<div class='well'>" + frontEndReq + "</div></div>" +
         "<div id='backEnd' class='taskBox'>Back<div class='well'>" + backEndReq + "</div></div>" +
         "<div id='logic' class='taskBox'>Logic<div class='well'>" + logicReq + "</div></div>");
-    $("#projectOverviewDiv").append("<div class='btn btn-lg btn-default'>Assign Staff</div>");
+    $("#projectOverviewDiv").append("<div id='assignStaffBtn' class='btn btn-lg btn-default'>Assign Staff</div>");
 }
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function assignStaff(){
+    while(frontEndEmployee < 1 && backEndEmployee < 1 && logicEmployee < 1) {
+        $.ajax(
+
+        )
+    }
 }
 
