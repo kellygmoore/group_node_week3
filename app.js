0745d06c53073a053fbc13ac05c610fa60e7a486
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-10-28 16:31:50 -0500

    add basic node server

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 /**
  * Created by danesmith on 10/28/15.
  */
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.get('') NEED URL FROM AJAX CALL
+
+app.get("/*", function(request,response) {
+    var file = request.params[0] || "index.html";
+
+    response.sendFile(path.join(__dirname, "./public/", file));
+});
+
+app.listen(app.get("port"));
