904106a1d9dc2f2af9b6e97a8ce5b40d0035d678
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2015-10-28 16:37:51 -0500

    code comments

--- public/app.js
+++ public/app.js
@@ -8,17 +8,22 @@ var backEndEmployee = 0, frontEndEmployee = 0, logicEmployee = 0;
 //total number of points in each scrum category
 var frontEndTotal, backEndTotal, logicTotal;
 
+
+//jQuery call
 $(document).ready(function(){
+    //Calls the generateProject(generates project requirements) function on click of the generate function button.
     $("#genButtonDiv").on('click', generateProject)
+    //Calls the assignstaff (generate employees untill there is one in each category) function on click of the assign staff button
     $("#projectOverviewDiv").on('click', '#assignStaffBtn', assignStaff)
 
 });
 
 
-
-
+//Function that sets the scrum point requirements for the project, creates thee columns for each team
+//and populates those columns with the point requirements. Also creates the assign staff button.
 function generateProject(){
     $("#projectOverviewDiv").empty();
+    //Point requirements for each team.
     frontEndReq = randomNumber(10,60);
     backEndReq = randomNumber(10,60);
     logicReq = randomNumber(10,60);
@@ -27,11 +32,11 @@ function generateProject(){
         "<div id='logic' class='taskBox'>Logic<div class='well'>" + logicReq + "</div></div>");
     $("#projectOverviewDiv").append("<div id='assignStaffBtn' class='btn btn-lg btn-default'>Assign Staff</div>");
 }
-
+//Random number generator.
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
-
+//Creates random employees from the server as long as there is at least one empty team.
 function assignStaff(){
     while(frontEndEmployee < 1 && backEndEmployee < 1 && logicEmployee < 1) {
         $.ajax(
@@ -39,4 +44,21 @@ function assignStaff(){
         )
     }
 }
+//Calculates which team has the largest ration of scrum requirements to scrum points and calculates
+//the number of weeks it will take that team to finish, and thus for the project to reach completion.
+function calculateProjectTime(){
+    var back = 0, front = 0, logic = 0;
+
+
+
+    if(front > back && front > logic){
+        return front;
+    } else if (back>front && back > logic){
+        return back;
+    } else {
+        return logic;
+    }
+
+}
+
 
