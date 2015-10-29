bc92edd16fc98e081c35dbb8097da13944be1c9a
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2015-10-29 11:13:56 -0500

    fixed team divs

--- server/name.js
+++ server/name.js
@@ -2,14 +2,12 @@
  * Created by danesmith on 10/28/15.
  */
 
-<<<<<<< HEAD
-var peopleArray = ["Kelly", "Dane", "Sam", "Aron", "Jason", "Dana", "Paul", "Paige", "Zeeshan", "Laryssa", "Heather", "Jake"];
-=======
+
 var peopleArray = ["Irene", "Nicole", "Tina", "Ryan", "Joe", "Helen", "Evelyn", "Denise", "Nicole", "Phillip", "Ruth", "Anne", "Emily",
     "Judith","Joshua","Anna","Tina","John","Kimberly","Martha","Anna","Donna","Juan","Ernest","Michelle","Gloria","Henry","Joyce",
     "Joan","Robert","Jean","Michelle","Kathleen","Jimmy","Lawrence","Sam","Dane","Heather","Paige","Kelly", "Andy", "Mike", "Scott", "Kris", "Danny", "Pui", "Aron"
 ];
->>>>>>> 01d82c7a278d49566c9377354f45ddf7e3dffa44
+
 
 
 //Random number generator.
@@ -20,13 +18,10 @@ function randomNumber(min, max){
 
 
 var whichPerson = function(){
-<<<<<<< HEAD
-    var getPersonIndex = randomNumber(0,11);
-    return peopleArray[getPersonIndex];
-=======
+
     var getPersonIndex = randomNumber(0, peopleArray.length-1);
     return peopleArray.splice(getPersonIndex, 1).toString()
->>>>>>> 01d82c7a278d49566c9377354f45ddf7e3dffa44
+
 };
 
 module.exports = whichPerson;
