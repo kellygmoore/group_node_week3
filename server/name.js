c99cdf1757cb71ac34eae1aaaa272eb6f8efefae
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-10-29 11:02:37 -0500

    array work

--- server/name.js
+++ server/name.js
@@ -2,8 +2,9 @@
  * Created by danesmith on 10/28/15.
  */
 
-var peopleArray = ["Irene", "Nicole", "Tina", "Ryan", "Joe", "Helen", "Evelyn", "Denise", "Nicole", "Phillip", "Ruth", "Anne", "Emily"
-
+var peopleArray = ["Irene", "Nicole", "Tina", "Ryan", "Joe", "Helen", "Evelyn", "Denise", "Nicole", "Phillip", "Ruth", "Anne", "Emily",
+    "Judith","Joshua","Anna","Tina","John","Kimberly","Martha","Anna","Donna","Juan","Ernest","Michelle","Gloria","Henry","Joyce",
+    "Joan","Robert","Jean","Michelle","Kathleen","Jimmy","Lawrence","Sam","Dane","Heather","Paige","Kelly"
 ];
 
 
@@ -16,7 +17,7 @@ function randomNumber(min, max){
 
 var whichPerson = function(){
     var getPersonIndex = randomNumber(0, peopleArray.length-1);
-    return peopleArray[getPersonIndex];
+    return peopleArray.splice(getPersonIndex, 1).toString()
 };
 
 module.exports = whichPerson;
